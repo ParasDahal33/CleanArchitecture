532ec204417d107fa53ec79f9ca6c7bfd7a52b5c
ParasDahal33 <paras.dahal2016@gmail.com>
2023-07-31 13:00:54 +0545

    Add TypeEvents

---
+++ src/Domain/Events/TypeEvent/TypeAddedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Events.TypeEvent;
+public class TypeAddedEvent : BaseEvent
+{
+    public TypeAddedEvent(CarType carType)
+    {
+        CarTypes = carType;
+    }
+
+    public CarType CarTypes { get; }
+}
---
+++ src/Domain/Events/TypeEvent/TypeDeletedEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Events.TypeEvent;
+public class TypeDeletedEvent : BaseEvent
+{
+    public TypeDeletedEvent(CarType carType)
+    {
+        CarTypes = carType;
+    }
+
+    public CarType CarTypes { get; }
+}
