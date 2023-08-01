11ad75e1c78da0594f5d195e08dadae0568104f5
ParasDahal33 <paras.dahal2016@gmail.com>
2023-08-01 10:57:41 +0545

    Add Create Type Command

---
+++ src/Application/Type/Commands/AddType/AddTypeCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace CleanArchitecture.Application.Type.Commands.AddType;
+public class AddTypeCommandValidator : AbstractValidator<AddTypeCommand>
+{
+    public AddTypeCommandValidator() 
+    {
+        RuleFor(v => v.Name)
+            .MaximumLength(256)
+            .NotEmpty();
+    }
+}
